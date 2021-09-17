f4c153df547312187bb482b8c196bf1976ac59a2
szilG <gombiszilvi@gmail.com>
2021-09-17 17:29:40 +0000

    removed secret key and set debug

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_bo10b=mv2uo&j$hpx39=&#j^zy-@8zg1^lb-yj%gt$z6)f)c3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['xtreme-fitness.herokuapp.com', 'localhost']
 
