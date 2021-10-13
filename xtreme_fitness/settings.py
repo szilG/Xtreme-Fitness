e67923a5e60932db83edca04e0b40d6bbbc0028f
szilG <gombiszilvi@gmail.com>
2021-10-13 14:13:08 +0000

    checking heroku allowed host problem

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from pathlib import Path
+
 import os
 import dj_database_url
-from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.environ.get('SECRET_KEY', '')
 SECRET_KEY = 'django-insecure-_bo10b=mv2uo&j$hpx39=&#j^zy-@8zg1^lb-yj%gt$z6)f)c3'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEVELOPMENT' in os.environ
+
+ALLOWED_HOSTS = ['xtreme-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +126,17 @@ WSGI_APPLICATION = 'xtreme_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
