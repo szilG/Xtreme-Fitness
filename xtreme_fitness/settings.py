39c74b262094cc8ef4a55bdd9add89889ac3fd1e
szilG <gombiszilvi@gmail.com>
2021-10-16 16:11:06 +0000

    base css and login/reg refactor and add favicon

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECRET_KEY = 'django-insecure-_bo10b=mv2uo&j$hpx39=&#j^zy-@8zg1^lb-yj%gt$z6)f)c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
