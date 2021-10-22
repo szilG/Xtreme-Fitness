780ed37cd4bdc270a5ba1360ce415d9f5509df66
szilG <gombiszilvi@gmail.com>
2021-10-22 16:25:46 +0000

    added cards to blog html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def blog(request):
     """A view to return the blog page"""
 
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
