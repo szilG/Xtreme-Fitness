dc7f5f14bfcb4f911f851d2eb2ebc571c8f15e6e
szilG <gombiszilvi@gmail.com>
2022-11-29 12:19:18 +0000

    add script file, will be referenced and run in the deployment stages on Render.com

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['xtreme-fitness.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = #['xtreme-fitness.herokuapp.com', 'localhost']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
