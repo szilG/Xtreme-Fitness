ed4d81aed2cf084dde6086a895c102b1168bedf2
szilG <gombiszilvi@gmail.com>
2022-11-29 12:50:48 +0000

    Delete unused host variable

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = #['xtreme-fitness.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
  if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
