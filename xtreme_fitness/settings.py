e52531b95cbe85caa42143a5e8cfa7ef185e6897
szilG <gombiszilvi@gmail.com>
2022-11-29 13:27:58 +0000

    fix syntax error

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -29,12 +29,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-#['xtreme-fitness.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = [
+
+ALLOWED_HOSTS = ['xtreme-fitness.herokuapp.com', 'localhost']
+# Add Render.com URL to allowed hosts
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
  if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-]
 
 
 # Application definition
