512e9149a2ecf1078a86d326922ce3c6df8026f7
szilG <gombiszilvi@gmail.com>
2022-11-29 13:19:33 +0000

    correct syntacs error

--- xtreme_fitness/settings.py
+++ xtreme_fitness/settings.py
@@ -30,10 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 #['xtreme-fitness.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = 
+ALLOWED_HOSTS = [
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
  if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+]
 
 
 # Application definition
